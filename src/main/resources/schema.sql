859e228d0881af0f236b1db461c5cbd7d09577d7
Aarenoksitolikus <super.naxa2001@yandex.ru>
2021-06-22 10:39:27 +0300

    added admin

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,3 +5,7 @@ CREATE TABLE IF NOT EXISTS persistent_logins (
     last_used TIMESTAMP   NOT NULL,
     PRIMARY KEY (series)
     );
+
+INSERT INTO account (id, email, hash_password, state, username, role, avatar)
+VALUES (12, 'sidikov.marsel@gmail.com', '$2a$10$5RP6GcyWspZ47uik1dLlzO9E.6O8sMM88ShX7cs9SeE59x/jZ3BlO', 'ACTIVE', 'Admin', 'ADMIN', 'https://shutniki.club/wp-content/uploads/2020/01/avatarka_dlya_admina_3_18122231.jpg')
+    ON CONFLICT (id) DO NOTHING;
