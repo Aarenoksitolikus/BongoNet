1910e572296786d83899f9f46330f63527dae2ba
Aarenoksitolikus <super.naxa2001@yandex.ru>
2021-06-22 10:44:17 +0300

    added admin cr date

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,6 +6,6 @@ CREATE TABLE IF NOT EXISTS persistent_logins (
     PRIMARY KEY (series)
     );
 
-INSERT INTO account (id, email, hash_password, state, username, role, avatar)
-VALUES (12, 'sidikov.marsel@gmail.com', '$2a$10$5RP6GcyWspZ47uik1dLlzO9E.6O8sMM88ShX7cs9SeE59x/jZ3BlO', 'ACTIVE', 'Admin', 'ADMIN', 'https://shutniki.club/wp-content/uploads/2020/01/avatarka_dlya_admina_3_18122231.jpg')
-    ON CONFLICT (id) DO NOTHING;
+INSERT INTO account (id, email, hash_password, state, username, role, avatar, creatiion_date)
+VALUES (12, 'sidikov.marsel@gmail.com', '$2a$10$5RP6GcyWspZ47uik1dLlzO9E.6O8sMM88ShX7cs9SeE59x/jZ3BlO', 'ACTIVE', 'Admin', 'ADMIN', 'https://shutniki.club/wp-content/uploads/2020/01/avatarka_dlya_admina_3_18122231.jpg', '2021-05-30')
+    ON CONFLICT (id) DO UPDATE SET creation_date = '2021-05-30';
